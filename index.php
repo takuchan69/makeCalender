18c0a525d081b6fcfb5a0ce645fcb37694f6307f
takuchan2468 <takuchan2468@penguin.lxd>
2020-06-10 15:40:03 +0900

    added html content

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
+<?php
 echo 'first step to make calendar';
 ?>
+<!DOCTYPE html>
+       <head>
+             <meta charset="UTF-8">
+             <meta name='keywords' content='practice for php'>
+             <title>calendar</title>
+       </head>
+       <body>
+             <header>
+                    <h1>Head Title of Calendar</h1>
+             </header>
+             <section class='container'>
+               
+             </section>
+       </body>
+</html>
