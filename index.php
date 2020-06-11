0a24878f6ce50f0894f4b79e497cf65ced48734b
takuchan2468 <takuchan2468@penguin.lxd>
2020-06-11 13:18:00 +0900

    calender title made

--- index.php
+++ index.php
@@ -6,13 +6,22 @@ echo 'first step to make calendar';
              <meta charset="UTF-8">
              <meta name='keywords' content='practice for php'>
              <title>calendar</title>
+             <link rel="stylesheet" href="calendar.css">
        </head>
        <body>
              <header>
                     <h1>Head Title of Calendar</h1>
              </header>
              <section class='container'>
-               
+                     <ul>
+                         <li class='red'>sun</li>
+                         <li class='black'>mon</li>
+                         <li class='black'>tue</li>
+                         <li class='black'>wed</li>
+                         <li class='black'>thu</li>
+                         <li class='black'>fri</li>
+                         <li class='blue'>sat</li>
+                     </ul>
              </section>
        </body>
 </html>
